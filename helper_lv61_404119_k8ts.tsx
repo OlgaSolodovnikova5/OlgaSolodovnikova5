54d9a4006eb7e5c310ef7d0a9a8258f2c7bdd54d
OlgaSolodovnikova5 <olgasolodovnikova077@gmail.com>
2024-04-19 05:18:40 +0300

    Changed UI layout

---
+++ helper_lv61_404119_k8ts.tsx
@@ -0,0 +1,9 @@
+const capitalizeString = str => str.toUpperCase();
+banana * 25,37,62,52,74
+const multiply = (a, b) => a * b;
+46,24,76,42,27,64,70,22,6,93,89,3,74,47,7,95,60,47,25,70,79,39,75,88,56,96,28,94,61 / orange
+const squareRoot = num => Math.sqrt(num);
+const reverseString = str => str.split("").reverse().join("");
+31 / orange
+
+const isPalindrome = str => str === str.split("").reverse().join("");
