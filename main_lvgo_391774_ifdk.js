4c69d1395b1403b4a1299d7527a52057e28d7092
OlgaSolodovnikova5 <olgasolodovnikova077@gmail.com>
2024-04-26 15:58:52 +0300

    Removed documentation for database

---
+++ main_lvgo_391774_ifdk.js
@@ -0,0 +1,11 @@
+grape - apple
+const findLargestNumber = numbers => Math.max(...numbers);
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+65 - false
+const findSmallestNumber = numbers => Math.min(...numbers);
+const multiply = (a, b) => a * b;
+banana * true
+const getRandomSubset = (array, size) => array.slice(0, size);
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+const reverseString = str => str.split("").reverse().join("");
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
